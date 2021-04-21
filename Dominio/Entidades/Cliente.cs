0f6c8f7c917ecd42fc9c9c6b27310f6cf36936d0
Tallys Witer <tallyswiter@hotmail.com>
2021-04-20 22:49:38 -0300

    Criação da entidade Cliente e entidade Conta. Todo: Conta - ( PagarConta(), SolicitarEmprestimo() ) Cliente - ( AtualizarDados(), NovoCliente()  )

---
+++ Dominio/Entidades/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio.Entidades
+{
+    public class Cliente : Base
+    {
+        public string Nome { get; set; }
+        
+        public string Cpf { get; set; }
+
+        public string Telefone { get; set; }
+
+        public Conta conta { get; set; }
+
+        public void AtualizarDados()
+        {
+            //Todo
+        }
+        public void NovoCliente()
+        {
+            //Todo
+        }
+    }
+}
---
+++ Dominio/Entidades/Conta.cs
@@ -0,0 +1,30 @@
+﻿using Dominio.ValuesType;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio.Entidades
+{
+    public class Conta : Base
+    {
+        public string NumeroConta { get; set; }
+
+        public EnumTipoConta Tipo { get; set; }
+
+        public double Saldo { get; set; }
+
+        public void AlterarTipo()
+        {
+            Tipo = Tipo == EnumTipoConta.Corrente ? EnumTipoConta.Poupança : EnumTipoConta.Corrente;  
+        }
+
+        public void PagarConta()
+        {
+            //Todo
+        }
+        public void SolicitarEmprestimo()
+        {
+            //Todo
+        }
+    }
+}
