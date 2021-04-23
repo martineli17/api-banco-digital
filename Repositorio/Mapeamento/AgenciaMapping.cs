36aa2c456a42705933e46cad9535cbbc5d3e58b5
Tallys Witer <tallyswiter@hotmail.com>
2021-04-23 20:01:40 -0300

    Criação da AgenciaMapping

---
+++ Repositorio/Mapeamento/AgenciaMapping.cs
@@ -0,0 +1,24 @@
+﻿using Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Repositorio.Mapeamento
+{
+    public class AgenciaMapping : IEntityTypeConfiguration<Agencia>
+    {
+        public void Configure(EntityTypeBuilder<Agencia> builder)
+        {
+            builder.ToTable("Deposito");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                   .HasColumnName("IdAgencia")
+                   .HasColumnType("UNIQUEIDENTIFIER")
+                   .IsRequired();
+            builder.Property(x => x.Numero)
+                   .HasColumnName("Numero")
+                   .HasColumnType("numeric")
+                   .IsRequired();
+        }
+    }
+}
