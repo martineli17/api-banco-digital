d940c96e348c0fe164d016b735099da5b5888ff8
felipearantes <felipe.arantes05@gmail.com>
2021-04-22 21:01:37 -0300

    Implementado validator para Conta e Agencia

---
+++ Dominio/Validators/EntidadesValidator/AgenciaValidator.cs
@@ -0,0 +1,18 @@
+﻿using Dominio.Entidades;
+using Dominio.Validators.MessagensValidator;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio.Validators.EntidadesValidator
+{
+    public class AgenciaValidator : AbstractValidator<Agencia>
+    {
+        public AgenciaValidator()
+        {
+            RuleFor(x => x.Numero).NotEmpty().WithMessage(MensagemValidator.NaoNuloOuVazio("Número"))
+                .MaximumLength(50).WithMessage(MensagemValidator.NaoMaior("Número"));
+        }
+    }
+}
---
+++ Dominio/Validators/EntidadesValidator/ContaValidator.cs
@@ -0,0 +1,21 @@
+﻿using Dominio.Entidades;
+using Dominio.Validators.MessagensValidator;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio.Validators.EntidadesValidator
+{
+    public class ContaValidator : AbstractValidator<Conta>
+    {
+        public ContaValidator()
+        {
+            RuleFor(x => x.IdCliente).NotEmpty().WithMessage(MensagemValidator.ErroNoProcesso);
+            RuleFor(x => x.Numero).NotEmpty().WithMessage(MensagemValidator.NaoNuloOuVazio("Número"))
+                .MaximumLength(50).WithMessage(MensagemValidator.NaoMaior("Número"));
+            RuleFor(x => x.Saldo).NotNull().WithMessage(MensagemValidator.NaoNuloOuVazio("Saldo")).
+                NotEmpty().WithMessage(MensagemValidator.ErroNoProcesso);
+        }
+    }
+}
