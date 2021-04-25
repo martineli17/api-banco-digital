3c449c9962bbbd5c340bb26d35894f6063c7fd17
felipearantes <felipe.arantes05@gmail.com>
2021-04-25 17:57:19 -0300

    Implementado repositorio conta e agencia

---
+++ Repositorio/Repositorios/AgenciaRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using Dominio.Entidades;
+using Repositorio.Repositorios.Bases;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositorio.Repositorios
+{
+    public class AgenciaRepositorio : BaseRepositorio<Agencia>
+    {
+        public AgenciaRepositorio(InjectorRepositorio injector) : base(injector)
+        {
+        }
+    }
+}
---
+++ Repositorio/Repositorios/ContaRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using Dominio.Entidades;
+using Repositorio.Repositorios.Bases;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositorio.Repositorios
+{
+    public class ContaRepositorio : BaseRepositorio<Conta>
+    {
+        public ContaRepositorio(InjectorRepositorio injector) : base(injector)
+        {
+        }
+    }
+}
