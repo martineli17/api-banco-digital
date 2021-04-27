3cc8abc8b843232d9f3a056646fd3edc4eefac61
Tallys Witer <tallyswiter@hotmail.com>
2021-04-27 19:36:42 -0300

    Criado as classes CartaoRepositorio e ClienteRepositorio.

---
+++ Repositorio/Repositorios/CartaoRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using Dominio.Entidades;
+using Dominio.Entidades.Bases;
+using Repositorio.Repositorios.Bases;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositorio.Repositorios
+{
+    public class CartaoRepositorio : BaseRepositorio<Conta>
+    {
+        public CartaoRepositorio(InjectorRepositorio injector) : base(injector)
+        {
+        }
+    }
+}
